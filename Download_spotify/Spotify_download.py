83979bfd50bcc1794e1b5fe5a6effc8b50f01838
Aayush Paudel <90232393+Unknown-333@users.noreply.github.com>
2023-07-06 14:54:14 +0545

    Update Spotify_download.py

--- Download_spotify/Spotify_download.py
+++ Download_spotify/Spotify_download.py
@@ -1,11 +1,4 @@
-"""
- Before running the code, make sure you have the requests library installed (pip install requests) 
-and yt-dlp installed and accessible from the command line.
 
-Remember to replace the playlist_id and api_key variables with your own values.
- Additionally, specify a valid directory in the dir variable where you want the downloaded files 
- to be saved.
-"""
 
 import requests
 import subprocess
